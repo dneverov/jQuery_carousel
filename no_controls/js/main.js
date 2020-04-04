3e90729f15e1867e078abf46fe50dc04227d233f
Denis Neverov <denis.neverov@gmail.com>
2020-04-04 04:02:57 +0300

    Add carousel controls

--- no_controls/js/main.js
+++ no_controls/js/main.js
@@ -16,4 +16,38 @@ $(document).ready(function(){
       $firstSlide.css({marginLeft: 0});
     })
   }
+
+  // Arrow Controls
+  $('#left-arrow').click(previousSlide);
+  $('#right-arrow').click(nextSlide);
+  $('.slide-image').click(nextSlide);
+
+  function nextSlide(){
+    // Go to next slide
+    window.clearInterval(interval);
+    var $currentSlide = $('#carousel').find('div:first');
+    var width = $currentSlide.width();
+
+    $currentSlide.animate({marginLeft: -width}, 1000, function(){
+      // What to do after the animation
+      var $lastSlide = $('#carousel').find('div:last');
+      $lastSlide.after($currentSlide);
+      $currentSlide.css({marginLeft: 0});
+      interval = window.setInterval(rotateSlides, 3000);
+    })
+  }
+
+  function previousSlide(){
+    // Go to previous slide
+    window.clearInterval(interval);
+    var $currentSlide = $('#carousel').find('div:first');
+    var width = $currentSlide.width();
+    var $previousSlide = $('#carousel').find('div:last');
+    $previousSlide.css({marginLeft: -width});
+    $currentSlide.before($previousSlide);
+    $previousSlide.animate({marginLeft: 0}, 1000, function(){
+      // What to do after the animation
+      interval = window.setInterval(rotateSlides, 3000);
+    });
+  }
 })
